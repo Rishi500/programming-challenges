fbba14b48b3fe0bc34f906982c89e51e07f3addf
Rishi Jain <rishi.jain500@gmail.com>
2022-05-09 22:23:16 +0530

    fix bug for currying with placeholder

--- 2_curry_placeholder.js
+++ 2_curry_placeholder.js
@@ -1,14 +1,15 @@
 function curry(f) {
   return function allArgs(...args1) {
     const complete =
-      args1.length >= f.length && !args1.includes(curry.placeholder);
+      args1.length >= f.length && !args1.slice(0,f.length).includes(curry.placeholder);
     if (complete) {
       return f(...args1);
     } else {
       return function (...args2) {
-        const rest = args1.map((arg) =>
+        const rest = args1.map((arg) => 
           arg === curry.placeholder && args2.length ? args2.shift() : arg
         );
+        console.log(...rest, ...args2)
         return allArgs(...rest, ...args2);
       };
     }
@@ -25,3 +26,4 @@ const _ = curry.placeholder;
 curriedJoin(_, 2)(1, 3); // '1_2_3'
 curriedJoin(1,_,3)(_)(2)
 curriedJoin(_,_,_)(_,_)(1,_)(_,3)(2)
+curriedJoin(_,_,_,_)(_,2,_)(_,3)(1)
